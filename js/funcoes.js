b051da3aa985d8686f41e352ed9525a15c11cd9e
Judeoddeus <julianaacademi@gmail.com>
2021-03-11 20:00:22 -0300

    commit

--- js/funcoes.js
+++ js/funcoes.js
@@ -3,7 +3,7 @@ let fotos = ["foto1.jpg", "foto2.jpg", "foto3.jpg", "foto4.jpg"];
 
 
 function TrocarFoto(foto){
-    document.querySelector(".banner-rotativo").src = "images/" + fotos[foto];
+    document.querySelector(".banner-rotativo").src = "images/slider" + fotos[foto];
 }
 
 let fotoAtual = 0;
